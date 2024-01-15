7e8e846b25cd01cd701332de2881aa8869e0d95f
schef <zvanstefan@gmail.com>
2024-01-15 09:52:40 +0100

    set credentials to e

--- scripts/credentials.py
+++ scripts/credentials.py
@@ -1,5 +1,7 @@
-db_user="crbs_user"
-db_password=""
-db_host="127.0.0.1"
-db_port=3306
-db_database="crbs_db"
+import os
+
+db_user = os.getenv("MARIADB_DATABASE", "crbs_user")
+db_password =os.getenv("MARIADB_PASSWORD", "6vTccw7zYF")
+db_host =os.getenv("crbs-mariadb", "127.0.0.1")
+db_port = 3306
+db_database = os.getenv("MARIADB_DATABASE", "crbs_db")
